465ade16fbffb56f86987a6be65158125c9fc467
Heveliev-destress <tolik05.03@ukr.net>
2023-01-11 13:27:11 +0200

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+player.on('timeupdate',throttle(onPlay,1000));
+
+
+function onPlay (data){
+const setLocal = localStorage.setItem('videoplayer-current-time',JSON.stringify(data))
+
+}
+const timeNow = JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds;
+player.setCurrentTime(timeNow)
 
